ded81eb59445c8f1b90373f59b648556afb2883f
Scott Scoble <scott@scoble.me>
2023-07-30 12:54:37 -0700

    start making this a resume-only repo

--- devenv.nix
+++ devenv.nix
@@ -13,8 +13,8 @@
 
   scripts = {
     install-ctan-dependencies.exec = ''
-      mkdir -p $DEVENV_ROOT/Resume/.texmf
-      pushd $DEVENV_ROOT/Resume/.texmf
+      mkdir -p $DEVENV_ROOT/.texmf
+      pushd $DEVENV_ROOT/.texmf
       if [[ ! -d enumitem ]]; then
         wget https://mirrors.ctan.org/macros/latex/contrib/enumitem.zip
         unzip enumitem.zip
@@ -44,6 +44,42 @@
       TEXINPUTS=".texmf//:" xelatex resume.tex && \
       pandoc -s resume.tex -o resume.docx 
       popd
+    '';
+
+    next-tag.exec = ''
+      # Get the latest tag
+      CURRENT_TAG=$(git describe --tags --abbrev=0)
+      # Get the hash of the experience section in the current commit and the previous one
+      CURRENT_HASH=$(yq '.experience' resume.yaml | sha256sum | awk '{print $1}')
+      PREVIOUS_HASH=$(git show $CURRENT_TAG:resume.yaml | yq '.experience' | sha256sum | awk '{print $1}')
+      
+      # Get the size of the experience list in the current commit and the previous one
+      CURRENT_SIZE=$(yq '.experience | length' resume.yaml)
+      PREVIOUS_SIZE=$(git show $CURRENT_TAG:resume.yaml | yq '.experience | length')
+
+      increment="patch"
+      
+      # Check the sizes and hashes to determine the version increment
+      if [ "$CURRENT_SIZE" -ne "$PREVIOUS_SIZE" ]; then
+        # If the size of the experience list has changed, increment the major version
+        increment="major"
+      elif [ "$CURRENT_HASH" != "$PREVIOUS_HASH" ]; then
+        # If the hash of the experience section has changed, increment the minor version
+        increment="minor"
+      fi
+
+      IFS='.' read -ra VERSION_PARTS <<< "$version"
+      case $increment in
+        major)
+          echo "new_version=$((''${VERSION_PARTS[0]} + 1)).0.0"
+          ;;
+        minor)
+          echo "new_version=''${VERSION_PARTS[0]}.$((''${VERSION_PARTS[1]} + 1)).0"
+          ;;
+        patch)
+          echo "new_version=''${VERSION_PARTS[0]}.''${VERSION_PARTS[1]}.$((''${VERSION_PARTS[2]} + 1))"
+          ;;
+      esac
     '';
   };
 }
