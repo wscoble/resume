9473fe18ea6188acb4011fb41d8e265e2200d77b
Scott Scoble <scott@scoble.me>
2023-07-30 00:41:26 -0700

    update template and dependencies

--- devenv.nix
+++ devenv.nix
@@ -18,22 +18,49 @@
       if [[ ! -d enumitem ]]; then
         wget https://mirrors.ctan.org/macros/latex/contrib/enumitem.zip
         unzip enumitem.zip
+        rm enumitem.zip
       fi
-      if [[ ! -d roboto ]]; then
-        wget https://mirrors.ctan.org/fonts/roboto.zip
-        unzip roboto.zip
+      if [[ ! -d titlesec ]]; then
+        wget https://mirrors.ctan.org/macros/latex/contrib/titlesec.zip
+        unzip titlesec.zip
+        rm titlesec.zip
       fi
-      if [[ ! -d fontaxes ]]; then
-        wget https://mirrors.ctan.org/macros/latex/contrib/fontaxes.zip
-        unzip fontaxes
+      if [[ ! -d tcolorbox ]]; then
+        wget https://mirrors.ctan.org/macros/latex/contrib/tcolorbox.zip
+        unzip tcolorbox.zip
+        rm tcolorbox.zip
       fi
+      # if [[ ! -d environ ]]; then
+      #   wget https://mirrors.ctan.org/macros/latex/contrib/environ.zip
+      #   unzip environ.zip
+      #   rm environ.zip
+      #   pushd environ
+      #   latex environ.ins
+      #   popd
+      # fi
+      # if [[ ! -d trimspaces ]]; then
+      #   wget https://mirrors.ctan.org/macros/latex/contrib/trimspaces.zip
+      #   unzip trimspaces.zip
+      #   rm trimspaces.zip
+
+      #   if [[ ! -d pstool ]]; then
+      #     wget https://mirrors.ctan.org/macros/latex/contrib/pstool.zip
+      #     unzip pstool.zip
+      #     rm pstool.zip
+      #   fi
+      #   pushd trimspaces
+      #   ln -s ../pstool/pstool.tex
+      #   ln -s ../titlesec/titlesec.sty
+      #   latex trimspaces.ins
+      #   popd
+      # fi
       popd
     '';
     
     build-resume.exec = ''
       pushd $DEVENV_ROOT/Resume
       j2 resume.j2 resume.yaml -o resume.tex && \
-      TEXINPUTS=".texmf//:" pdflatex resume.tex && \
+      TEXINPUTS=".texmf//:" xelatex resume.tex && \
       pandoc -s resume.tex -o resume.docx 
       popd
     '';
